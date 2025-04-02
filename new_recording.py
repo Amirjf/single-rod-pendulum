7e71f2eb9854852e9acb686b39ef7a31c37d0c5f
Amir <amirmasoodjafari1677@gmail.com>
2025-04-02 17:34:29 +0200

    Remove outdated data_new.csv file and update new_recording.py to write to half_theta.csv with modified header and time logging.

--- new_recording.py
+++ new_recording.py
@@ -4,20 +4,24 @@ import time
 
 ser = serial.Serial('/dev/cu.usbserial-0001', 115200)
 
-with open("data_new.csv", "w", newline="") as file:
+# 163
+# 97
+# 74
+# 30
+
+with open("half_theta.csv", "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["time", "xAccl", "yAccl", "zAccl", "encoder"])  # Updated header to include time
+    writer.writerow(["xAccl", "yAccl", "zAccl", "newPosition", "time",'pcTime'])  # Updated header to include time
     
+    start_time = time.time()  
     while True:
         try:
-            line = ser.readline().decode().strip()
+            line = ser.readline().decode('ISO-8859-1').strip()
             values = line.split(",")
+            current_time = (time.time() - start_time) * 1000
+            writer.writerow(values + [current_time])
             print(values)
-            if len(values) == 5:  # Ensure we have all 4 values
-                current_time = time.time()  # Get the current time
-                writer.writerow([current_time] + values)  # Prepend time to the values
-                time.sleep(0.005)
-                print([current_time] + values)
+
         except KeyboardInterrupt:
             print("Stopped by user")
             break
