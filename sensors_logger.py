4680e0703cf9ee7c4e25eef5341e48149c27d391
Amir <amirmasoodjafari1677@gmail.com>
2025-04-04 18:37:18 +0200

    Remove obsolete files including Python scripts for basic programs, pendulum simulations, flowcharts, and various CSV data files. This cleanup helps streamline the project by eliminating unused resources.

--- sensors_logger.py
+++ sensors_logger.py
@@ -9,7 +9,6 @@ ser = serial.Serial('/dev/cu.usbserial-0001', 115200)
 # 74
 # 30
 
-
 with open("test_down_asdasasdsimproved.csv", "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["xAccl", "yAccl", "zAccl", "newPosition", "time",'pcTime'])  # Updated header to include time
