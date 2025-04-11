bf1a66bf6b36690c70ab4c66196a94ce04606751
Soheil Behnam <123129446+soheilbh@users.noreply.github.com>
2025-04-11 05:24:41 +0200

    file to printing sequence

---
+++ sequence_output.py
@@ -0,0 +1,9 @@
+sequence = [
+    (0.000, ('left', 250)),      # Initial strong push
+    (0.300, ('left', 272)),      # First follow-up push
+    (0.622, ('right', 102)),      # Second follow-up push
+    (0.774, ('right', 245)),      # Third follow-up push
+    (1.069, ('right', 105)),      # Fourth follow-up push
+    (1.224, ('left', 116)),      # Fifth follow-up push
+    (1.390, ('right', 100)),      # Final push
+]
